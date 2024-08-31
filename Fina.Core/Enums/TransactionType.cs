76ecfa81312276ce9f68ebfbf8a0b90ba6546e77
Robson de Moraes Silva <rms2307@outlook.com>
2024-08-31 00:31:46 -0300

    Adiciona os projetos iniciais.

---
+++ Fina.Core/Enums/TransactionType.cs
@@ -0,0 +1,8 @@
+﻿namespace Fina.Core.Enums
+{
+    public enum TransactionType
+    {
+        Deposit = 1,
+        Withdraw = 2,
+    }
+}
---
+++ Fina.Core/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace Fina.Core.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? PaidOrReceivedAt { get; set; }
+
+        public bool Type { get; set; }
+
+        public long CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+
+        public string UserId { get; set; } = string.Empty;
+    }
+}
