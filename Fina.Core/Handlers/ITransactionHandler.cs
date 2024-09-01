694b1fc1d06e7310260bf6600ccdfd800bcb6dce
Robson de Moraes Silva <rms2307@outlook.com>
2024-08-31 23:45:34 -0300

    Cria projeto web

--- Fina.Core/Handlers/ITransactionHandler.cs
+++ Fina.Core/Handlers/ITransactionHandler.cs
@@ -1,6 +1,16 @@
-﻿namespace Fina.Core.Handlers
+﻿using Fina.Core.Models;
+using Fina.Core.Requests.Categories;
+using Fina.Core.Requests.Transactions;
+using Fina.Core.Responses;
+
+namespace Fina.Core.Handlers
 {
     public interface ITransactionHandler
     {
+        Task<Response<Transaction>> CreateAsync(CreateTransactionRequest request);
+        Task<Response<Transaction>> UpdateAsync(UpdateTransactionRequest request);
+        Task<Response<Transaction>> DeleteAsync(DeleteTransactionRequest request);
+        Task<Response<Transaction>> GetByIdAsync(GetByIdTransactionRequest request);
+        Task<PagedResponse<List<Transaction>>> GetAllAsync(GetTransactionsByPeriodRequest request);
     }
 }
