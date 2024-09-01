866e7165ae0a941cb485f1dc728335a54e312bcb
Robson de Moraes Silva <rms2307@outlook.com>
2024-09-01 00:29:51 -0300

    Faz mapeamentos

---
+++ Fina.Api/Infra/Mappings/CategoryMapping.cs
@@ -0,0 +1,29 @@
+﻿using Fina.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fina.Api.Infra.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Description)
+                .IsRequired(false)
+                .HasMaxLength(255);
+
+            builder.Property(x => x.UserId)
+                .IsRequired()
+                .HasMaxLength(160);
+        }
+    }
+}
---
+++ Fina.Api/Infra/Mappings/TransactionMapping.cs
@@ -0,0 +1,37 @@
+﻿using Fina.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fina.Api.Infra.Mappings
+{
+    public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transaction");
+
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Type)
+                .IsRequired();
+
+            builder.Property(x => x.Amount)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.Property(x => x.PaidOrReceivedAt)
+                .IsRequired();
+
+            builder.Property(x => x.UserId)
+                .IsRequired()
+                .HasMaxLength(160);
+        }
+    }
+}
